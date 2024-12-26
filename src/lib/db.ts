541b0ceeaeb5a5c2c491d798668d5e4a414eec0d
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-12-26 13:36:35 +0100

    feat(prisma): initialize prisma in project

---
+++ src/lib/db.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+function prismaClientSingleton() {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prisma: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = prismaClientSingleton()
+} else {
+  if (!globalThis.prisma) {
+    globalThis.prisma = prismaClientSingleton()
+  }
+  prisma = globalThis.prisma
+}
+
+export default prisma
